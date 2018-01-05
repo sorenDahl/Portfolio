efae5e821cc11b06d5dc62be61e644b183ab164d
blodbad123 <sdahlpoulsen@gmail.com>
2018-01-05 18:31:02 +0100

    Første

---
+++ images/skills.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas');
+
+console.log("lol!"); 
+
+// context variable
+var c = canvas.getContext("2d");
---
+++ scripts/skills2.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('haha');
+
+var c = canvas.getContext('2d');
+
+
+var colorArray = [
+    '#F34A53',
+    '#FAE3B4',
+    '#AAC789',
+    '#437356', 
+    '#1E4147'
+];
+
